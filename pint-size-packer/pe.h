797ebc11e49bb81bc88d3293dd693477a3c0e667
uafio <tschoranov@gmail.com>
2020-09-06 18:34:57 -0700

    refactor PE to reuse code and remove duplicate

--- pint-size-packer/pe.h
+++ pint-size-packer/pe.h
@@ -30,166 +30,58 @@ public:
     }
 };
 
-class PEFile
+class Sections
 {
 private:
-    void* data;
-    size_t size;
     std::vector< Section* > sections;
-    PEFile( const PEFile& );
-
-    bool get_file_content( const char* fpath )
-    {
-        std::ifstream file( fpath, std::fstream::binary | std::fstream::ate );
-        if ( !file.is_open() ) {
-            return false;
-        }
-
-        size = file.tellg();
-        data = calloc( size, 1 );
-
-        file.seekg( std::fstream::beg );
-        file.read( reinterpret_cast< char* >( data ), size );
-
-        return true;
-    }
-
-    void sections_update( void )
-    {
-        uint32_t raw = size_of_headers();
-        uint32_t rav = align_section( raw );
-
-        for ( auto section : sections ) {
-            section->hdr.PointerToRawData = raw;
-            section->hdr.VirtualAddress = rav;
-
-            raw += section->hdr.SizeOfRawData;
-            rav += align_section( section->hdr.Misc.VirtualSize );
-        }
-    }
-
-    template< class T >
-    T align_up( T addr, size_t alignment )
-    {
-        size_t pad = (uintptr_t)addr % alignment;
-        if ( pad ) {
-            return (uintptr_t)addr + alignment - pad;
-        }
-        return addr;
-    }
-
-
-    PIMAGE_SECTION_HEADER section_hdr( uint32_t index )
-    {
-        return IMAGE_FIRST_SECTION( nt_hdr() ) + index;
-    }
-
-
-    PIMAGE_SECTION_HEADER section_hdr( const char* name )
-    {
-        for ( int i = 0; i < file_hdr()->NumberOfSections; i++ ) {
-            auto section = section_hdr( i );
-
-            if ( strncmp( name, reinterpret_cast< char* >( section->Name ), sizeof( section->Name ) ) == 0 ) {
-                return section;
-            }
-        }
-        return nullptr;
-    }
-
-    void* section_data( uint32_t index )
-    {
-        return reinterpret_cast< char* >( data ) + section_hdr( index )->PointerToRawData;
-    }
-
-
 
 public:
-    PEFile( const char* fpath )
-        : data( nullptr ), size( 0 )
+    Sections( void )
     {
-        if ( get_file_content( fpath ) ) {
-            for ( int i = 0; i < file_hdr()->NumberOfSections; i++ ) {
-                auto shdr = section_hdr( i );
-                auto sdata = section_data( i );
-                sections.push_back( new Section( shdr, sdata, shdr->SizeOfRawData ) );
-            }
-        }
+        sections.reserve( 20 );
     }
 
-    ~PEFile( void )
+    ~Sections( void )
     {
-        if ( data ) {
-            free( data );
-            data = nullptr;
-            size = 0;
-        }
+        clear();
+    }
 
-        for ( auto sec : sections ) {
-            delete sec;
+    void clear( void )
+    {
+        for ( auto section : sections ) {
+            delete section;
         }
 
         sections.clear();
     }
 
-    std::vector< Section* >& get_sections( void )
+    void add( Section* section )
+    {
+        sections.push_back( section );
+    }
+
+    std::vector< Section* >& get( void )
     {
         return sections;
     }
 
-    PIMAGE_DOS_HEADER pe_hdr( void )
+    Section* pop( const char* name )
     {
-        return reinterpret_cast< PIMAGE_DOS_HEADER >( data );
-    }
-
-    PIMAGE_NT_HEADERS64 nt_hdr( void )
-    {
-        return reinterpret_cast< PIMAGE_NT_HEADERS64 >( reinterpret_cast< uintptr_t >( data ) + pe_hdr()->e_lfanew );
-    }
-
-    PIMAGE_FILE_HEADER file_hdr( void )
-    {
-        return &nt_hdr()->FileHeader;
-    }
-
-    PIMAGE_OPTIONAL_HEADER64 optional_hdr( void )
-    {
-        return &nt_hdr()->OptionalHeader;
-    }
-
-    PIMAGE_DATA_DIRECTORY data_dir( size_t index )
-    {
-        return &optional_hdr()->DataDirectory[index];
-    }
-
-
-    size_t rva2fo( size_t rva )
-    {
-        if ( rva <= section_hdr( 0U )->PointerToRawData ) {
-            return rva;
-        }
-
-        for ( int i = 0; i < file_hdr()->NumberOfSections; i++ ) {
-            auto section = section_hdr( i );
-
-            if ( rva >= section->VirtualAddress && rva < section->VirtualAddress + section->Misc.VirtualSize ) {
-                rva -= section->VirtualAddress;
-
-                if ( rva >= section->SizeOfRawData ) {
-                    return section->PointerToRawData;
-                }
-                return rva + section->PointerToRawData;
+        for ( auto it = sections.begin(); it != sections.end(); it++ ) {
+            if ( strncmp( name, reinterpret_cast< char* >( ( *it )->hdr.Name ), 8 ) == 0 ) {
+                Section* result = *it;
+                sections.erase( it );
+                return result;
             }
         }
 
-        return 0;
+        return nullptr;
     }
 
-
-    Section* section( const char* name )
+    Section* operator[]( const char* name )
     {
         for ( auto section : sections ) {
-            if ( strncmp( name, reinterpret_cast< char* >( section->hdr.Name ), sizeof( section->hdr.Name ) ) == 0 ) {
+            if ( strncmp( name, reinterpret_cast< char* >( section->hdr.Name ), 8 ) == 0 ) {
                 return section;
             }
         }
@@ -197,87 +89,67 @@ public:
         return nullptr;
     }
 
-    bool section_add( PIMAGE_SECTION_HEADER shdr, void* sdata )
+    bool rename( const char* name, const char* newname )
     {
-        PIMAGE_SECTION_HEADER lastsechdr = &sections.back()->hdr;
-
-        shdr->VirtualAddress = align_section( lastsechdr->VirtualAddress + lastsechdr->Misc.VirtualSize );
-        shdr->PointerToRawData = align_file( lastsechdr->PointerToRawData + lastsechdr->SizeOfRawData );
-
-        sections.push_back( new Section( shdr, sdata, shdr->SizeOfRawData ) );
-
-        file_hdr()->NumberOfSections++;
-        optional_hdr()->SizeOfImage += align_section( shdr->Misc.VirtualSize );
-        optional_hdr()->SizeOfHeaders = (DWORD)size_of_headers();
-
-        return true;
-    }
-
-    uint32_t size_of_headers( void )
-    {
-        uint32_t result = 0;
-
-        result += pe_hdr()->e_lfanew;
-        result += sizeof( IMAGE_NT_HEADERS64 );
-        result += sizeof( IMAGE_SECTION_HEADER ) * (uint32_t)sections.size();
-
-        return align_file( result );
-    }
-
-    uint32_t align_file( uint32_t value )
-    {
-        return (uint32_t)align_up< size_t >( value, optional_hdr()->FileAlignment );
-    }
-
-    uint32_t align_section( uint32_t value )
-    {
-        return (uint32_t)align_up< size_t >( value, optional_hdr()->SectionAlignment );
-    }
-
-    bool save( const char* fname )
-    {
-        std::ofstream ofile( fname, std::fstream::binary );
-        if ( !ofile.is_open() ) {
-            std::perror( __FUNCTION__ );
-            return false;
+        Section* section = this->operator[]( name );
+        if ( section ) {
+            memset( section->hdr.Name, 0, 8 );
+            strncpy( reinterpret_cast< char* >( section->hdr.Name ), newname, 8 );
+            return true;
         }
 
-        sections_update();
+        return false;
+    }
 
-        file_hdr()->NumberOfSections = (WORD)sections.size();
+    void rename( Section* section, const char* name )
+    {
+        memset( section->hdr.Name, 0, 8 );
+        strncpy( reinterpret_cast< char* >( section->hdr.Name ), name, 8 );
+    }
 
-        ofile.write( reinterpret_cast< char* >( pe_hdr() ), pe_hdr()->e_lfanew );
-        ofile.write( reinterpret_cast< char* >( nt_hdr() ), sizeof( IMAGE_NT_HEADERS64 ) );
+    void merge( void )
+    {
+        uint32_t rawsize = 0;
+        uint32_t vasz = 0;
+        for ( auto section : sections ) {
+            rawsize += section->hdr.SizeOfRawData;
+            vasz += section->hdr.Misc.VirtualSize;
+        }
+
+        uint8_t* data = reinterpret_cast< uint8_t* >( calloc( rawsize, 1 ) );
+
+        IMAGE_SECTION_HEADER hdr = sections.at( 0 )->hdr;
+
+        hdr.Characteristics = IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_WRITE;
+        hdr.Misc.VirtualSize = vasz;
+        hdr.SizeOfRawData = rawsize;
+
+        uint32_t offset = 0;
 
         for ( auto section : sections ) {
-            ofile.write( reinterpret_cast< char* >( &section->hdr ), sizeof( IMAGE_SECTION_HEADER ) );
+            memcpy( &data[offset], section->data, section->hdr.SizeOfRawData );
+            offset += section->hdr.SizeOfRawData;
         }
+        assert( offset == rawsize );
 
-        size_t pad = optional_hdr()->SizeOfHeaders - ofile.tellp();
-        while ( pad-- ) {
-            ofile.write( "\x00", 1 );
-        }
+        Section* newsection = new Section( &hdr, data, rawsize );
 
-        for ( auto section : sections ) {
-            ofile.write( reinterpret_cast< char* >( section->data ), section->hdr.SizeOfRawData );
-
-            pad = align_file( section->hdr.SizeOfRawData ) - section->hdr.SizeOfRawData;
-            while ( pad-- ) {
-                assert( false ); // Debugging: Should not happen
-                ofile.write( "\x00", 1 );
-            }
-        }
-
-        return true;
+        clear();
+        add( newsection );
     }
 };
 
 class PEHeader
 {
-private:
+protected:
     void* base;
 
 public:
+    __forceinline PEHeader( void )
+        : base( nullptr )
+    {
+    }
+
     __forceinline PEHeader( void* addr )
         : base( addr )
     {
@@ -335,6 +207,11 @@ public:
         return nullptr;
     }
 
+    __forceinline void* section_data( uint32_t index )
+    {
+        return reinterpret_cast< char* >( base ) + section_hdr( index )->PointerToRawData;
+    }
+
     template< class T >
     __forceinline T align_up( T addr, size_t alignment )
     {
@@ -343,5 +220,150 @@ public:
             return (uintptr_t)addr + alignment - pad;
         }
         return addr;
+    }
+
+    __forceinline size_t rva2fo( size_t rva )
+    {
+        if ( rva <= section_hdr( 0U )->PointerToRawData ) {
+            return rva;
+        }
+
+        for ( int i = 0; i < file_hdr()->NumberOfSections; i++ ) {
+            auto section = section_hdr( i );
+
+            if ( rva >= section->VirtualAddress && rva < section->VirtualAddress + section->Misc.VirtualSize ) {
+                rva -= section->VirtualAddress;
+
+                if ( rva >= section->SizeOfRawData ) {
+                    return section->PointerToRawData;
+                }
+                return rva + section->PointerToRawData;
+            }
+        }
+
+        return 0;
+    }
+};
+
+class PE : public PEHeader
+{
+private:
+    size_t size;
+
+    bool get_file_content( const char* fpath )
+    {
+        std::ifstream file( fpath, std::fstream::binary | std::fstream::ate );
+        if ( !file.is_open() ) {
+            return false;
+        }
+
+        size = file.tellg();
+        base = calloc( size, 1 );
+
+        file.seekg( std::fstream::beg );
+        file.read( reinterpret_cast< char* >( base ), size );
+
+        return true;
+    }
+
+public:
+    Sections sections;
+
+    PE( const char* fpath )
+        : size( 0 )
+    {
+        if ( get_file_content( fpath ) ) {
+            for ( int i = 0; i < file_hdr()->NumberOfSections; i++ ) {
+                auto shdr = section_hdr( i );
+                shdr->Misc.VirtualSize = align_section( shdr->Misc.VirtualSize );
+                auto sdata = section_data( i );
+                sections.add( new Section( shdr, sdata, shdr->SizeOfRawData ) );
+            }
+        }
+    }
+
+    ~PE( void )
+    {
+        if ( base ) {
+            free( base );
+            base = nullptr;
+            size = 0;
+        }
+    }
+
+    void update_headers( void )
+    {
+        uint32_t raw = size_of_headers();
+        uint32_t rav = align_section( raw );
+
+        for ( auto section : sections.get() ) {
+            section->hdr.PointerToRawData = raw;
+            section->hdr.VirtualAddress = rav;
+
+            raw += section->hdr.SizeOfRawData;
+            rav += align_section( section->hdr.Misc.VirtualSize );
+        }
+
+        file_hdr()->NumberOfSections = (WORD)sections.get().size();
+        optional_hdr()->SizeOfHeaders = size_of_headers();
+        optional_hdr()->SizeOfImage = sections.get().back()->hdr.VirtualAddress + sections.get().back()->hdr.Misc.VirtualSize;
+    }
+
+    uint32_t size_of_headers( void )
+    {
+        uint32_t result = 0;
+
+        result += pe_hdr()->e_lfanew;
+        result += sizeof( IMAGE_NT_HEADERS64 );
+        result += sizeof( IMAGE_SECTION_HEADER ) * (uint32_t)sections.get().size();
+
+        return align_file( result );
+    }
+
+    uint32_t align_file( uint32_t value )
+    {
+        return (uint32_t)align_up< size_t >( value, optional_hdr()->FileAlignment );
+    }
+
+    uint32_t align_section( uint32_t value )
+    {
+        return (uint32_t)align_up< size_t >( value, optional_hdr()->SectionAlignment );
+    }
+
+    bool save( const char* fname )
+    {
+        std::ofstream ofile( fname, std::fstream::binary );
+        if ( !ofile.is_open() ) {
+            std::perror( __FUNCTION__ );
+            return false;
+        }
+
+        update_headers();
+
+        file_hdr()->NumberOfSections = (WORD)sections.get().size();
+
+        ofile.write( reinterpret_cast< char* >( pe_hdr() ), pe_hdr()->e_lfanew );
+        ofile.write( reinterpret_cast< char* >( nt_hdr() ), sizeof( IMAGE_NT_HEADERS64 ) );
+
+        for ( auto section : sections.get() ) {
+            ofile.write( reinterpret_cast< char* >( &section->hdr ), sizeof( IMAGE_SECTION_HEADER ) );
+        }
+
+        size_t pad = optional_hdr()->SizeOfHeaders - ofile.tellp();
+        while ( pad-- ) {
+            ofile.write( "\x00", 1 );
+        }
+
+        for ( auto section : sections.get() ) {
+            ofile.write( reinterpret_cast< char* >( section->data ), section->hdr.SizeOfRawData );
+
+            pad = align_file( section->hdr.SizeOfRawData ) - section->hdr.SizeOfRawData;
+            while ( pad-- ) {
+                assert( false ); // Debugging: Should not happen
+                ofile.write( "\x00", 1 );
+            }
+        }
+
+        return true;
     }
 };
